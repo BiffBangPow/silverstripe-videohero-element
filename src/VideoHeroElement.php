cc2843942cfae0c315fcccc6dbd4d3ec79b68fa5
Tim Burt <tim@biffbangpow.com>
2022-11-08 10:57:34 +0000

    Update base code

---
+++ src/VideoHeroElement.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace BiffBangPow\Element;
+
+use DNADesign\Elemental\Models\BaseElement;
+use SilverStripe\AssetAdmin\Forms\UploadField;
+use SilverStripe\Assets\File;
+use SilverStripe\Assets\Image;
+use SilverStripe\Forms\DropdownField;
+use UncleCheese\DisplayLogic\Forms\Wrapper;
+
+class VideoHeroElement extends BaseElement
+{
+    private static $table_name = 'VideoHeroElement';
+    private static $inline_editable = false;
+    private static $has_one = [
+        'HeroImage' => Image::class,
+        'VideoFile' => File::class
+    ];
+    private static $owns = [
+        'HeroImage',
+        'VideoFile'
+    ];
+    private static $db = [
+        'Title' => 'Varchar',
+        'Content' => 'HTMLText',
+        'BannerType' => 'Varchar'
+    ];
+
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
+        $fields->removeByName(['VideoFile']);
+        $fields->addFieldsToTab('Root.Main', [
+            DropdownField::create('BannerType', 'Banner Type', [
+                'image' => 'Image Banner',
+                'video' => 'Video Banner'
+            ]),
+
+            UploadField::create('HeroImage', 'Image')
+                ->setAllowedFileCategories('image/supported')
+                ->setFolderName('hero')
+                ->setDescription('If the banner is set to video, this will be used as a poster image, so should still be added'),
+            Wrapper::create(
+                UploadField::create('VideoFile')
+                    ->setFolderName('hero-video')
+                    ->setAllowedExtensions(['webm', 'mp4'])
+                    ->setDescription('Video should be in mp4 or webm format, does not need sound and should be ideally less than around 20 seconds')
+            )->displayIf("BannerType")->isEqualTo("video")->end()
+        ]);
+        return $fields;
+    }
+
+    public function getType()
+    {
+        return _t(__CLASS__ . '.BlockType', 'Hero / Video Banner (with content)');
+    }
+}
---
+++ templates/BiffBangPow/Element/VideoHeroElement.ss
@@ -0,0 +1,38 @@
+<div class="hero-container py-4 bg-cover $BannerType"
+    <% if $BannerType == 'image' %>
+     style="background-image: url('<% if $Page.WebPSupport %>$HeroImage.ScaleMaxWidth(1920).Format('webp').Link')<% else %>$HeroImage.ScaleMaxWidth(1920).Link')<% end_if %>;"
+    <% end_if %>
+>
+    <div class="hero-overlay"></div>
+    <div class="container content-holder">
+        <div class="row">
+            <div class="col-12">
+                <div class="content">
+                    <% if $ShowTitle%>
+                        <h1 class="title">$Title</h1>
+                    <% end_if %>
+                    <% if $Content %>
+                        $Content
+                    <% end_if %>
+                </div>
+            </div>
+        </div>
+    </div>
+    <% if $BannerType == 'video' && $VideoFile %>
+        <video
+            <% if $HeroImage %>
+            poster="<% if $Page.WebPSupport %>
+                $HeroImage.ScaleMaxWidth(1920).Format('webp').URL
+            <% else %>
+                $HeroImage.ScaleMaxWidth(1920).URL
+            <% end_if %>"
+            <% end_if %>
+            autoplay playsinline muted loop>
+            <source src="$VideoFile.URL" type="$VideoFile.MimeType">
+        </video>
+    <% end_if %>
+</div>
+
+
+
+
